99da49df5c29123a196487bc351d4e51c71941a1
rayleigh420 <nhatduy0409@gmail.com>
2024-03-02 20:12:30 +0700

    FIX/ Option Theme

--- lib/presentation/my_app.dart
+++ lib/presentation/my_app.dart
@@ -28,8 +28,9 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: Strings.appName,
-          theme:
-              true ? AppThemeData.darkThemeData : AppThemeData.lightThemeData,
+          theme: _themeStore.darkMode
+              ? AppThemeData.darkThemeData
+              : AppThemeData.lightThemeData,
           routes: Routes.routes,
           locale: Locale(_languageStore.locale),
           supportedLocales: _languageStore.supportedLanguages
