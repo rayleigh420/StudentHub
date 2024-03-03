830b9c9654b128154a484d14be9d1b2560fd8899
tklongg <klong31122001@gmail.com>
2024-03-03 23:35:07 +0700

    ...

--- lib/presentation/signup/identity_signup/identity_signup.dart
+++ lib/presentation/signup/identity_signup/identity_signup.dart
@@ -20,7 +20,7 @@ class _SignUpIdentityState extends State<SignUpIdentity> {
   @override
   void initState() {
     super.initState();
-    _themeStore.changeBrightnessToDark(true);
+    // _themeStore.changeBrightnessToDark(true);
     x = 50;
     y = 50;
   }
