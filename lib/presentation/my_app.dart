94e0ea0cf41c8006477fa46cb8e3e25c6d82f752
tklongg <klong31122001@gmail.com>
2024-03-05 22:36:50 +0700

    change mage

--- lib/presentation/my_app.dart
+++ lib/presentation/my_app.dart
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
             // Built-in localization of basic text for Cupertino widgets
             GlobalCupertinoLocalizations.delegate,
           ],
-          home: AppBottomNavigationBar(),
+          home: InputLogin(),
         );
       },
     );
