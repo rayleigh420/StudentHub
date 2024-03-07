64378cc2773ebc8a89a7b9f32b5f394a9944c644
tklongg <klong31122001@gmail.com>
2024-03-07 12:58:21 +0700

    fix sth

--- lib/presentation/signup/input_signup/input_signup.dart
+++ lib/presentation/signup/input_signup/input_signup.dart
@@ -31,38 +31,40 @@ class _InputSignUpState extends State<InputSignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        // decoration: const BoxDecoration(color: Color(0xFFF6F6F6)),
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // const Text(
-            //   'StudentHub',
-            //   // textAlign: TextAlign.center,
-            //   style: TextStyle(
-            //       fontSize: 24,
-            //       //fontFamily: "GGX88HV",
-            //       fontWeight: FontWeight.bold),
-            // ),
-            const SizedBox(height: 17.0),
-            Text(
-              'Sign up as ${(widget.type == 1) ? 'Company' : 'Student'}',
-              // textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold
-                  //fontFamily: "GGX88Reg_Light",
-                  // color: Color(0xFF626262)
-                  ),
-            ),
-            const SizedBox(height: 20.0),
-            // const SizedBox(height: 17.0),
-            buildForm(context),
-            const SizedBox(height: 30),
-            buildLaunchButton(context, () {
-              log('Login pressed');
-            }),
-            buildSignupSection(context)
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          // decoration: const BoxDecoration(color: Color(0xFFF6F6F6)),
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // const Text(
+              //   'StudentHub',
+              //   // textAlign: TextAlign.center,
+              //   style: TextStyle(
+              //       fontSize: 24,
+              //       //fontFamily: "GGX88HV",
+              //       fontWeight: FontWeight.bold),
+              // ),
+              const SizedBox(height: 17.0),
+              Text(
+                'Sign up as ${(widget.type == 1) ? 'Company' : 'Student'}',
+                // textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold
+                    //fontFamily: "GGX88Reg_Light",
+                    // color: Color(0xFF626262)
+                    ),
+              ),
+              const SizedBox(height: 20.0),
+              // const SizedBox(height: 17.0),
+              buildForm(context),
+              const SizedBox(height: 30),
+              buildLaunchButton(context, () {
+                log('Login pressed');
+              }),
+              // buildSignupSection(context)
+            ],
+          ),
         ),
       ),
     );
@@ -208,7 +210,7 @@ class _InputSignUpState extends State<InputSignUp> {
             ),
           ),
         ),
-        const SizedBox(height: 16.0),
+        const SizedBox(height: 18.0),
         GestureDetector(
           onTap: () {
             setState(() {
@@ -217,25 +219,38 @@ class _InputSignUpState extends State<InputSignUp> {
           },
           child: Row(
             children: [
-              Checkbox(
-                value: _agreeToApply,
-                activeColor: const Color(0xFF4285F4),
-                checkColor: Colors.white,
-                onChanged: (value) {
-                  setState(() {
-                    _agreeToApply = value!;
-                  });
-                },
-              ),
-              Text(
-                'Yes, I understand and agree to StudentHub',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.normal,
-                  //fontFamily: "GGX88Reg_Light",
-                  color: textColor,
+              SizedBox(
+                height: 24,
+                width: 24,
+                child: Checkbox(
+                  value: _agreeToApply,
+                  activeColor: const Color(0xFF4285F4),
+                  checkColor: Colors.white,
+                  onChanged: (value) {
+                    setState(() {
+                      _agreeToApply = value!;
+                    });
+                  },
                 ),
               ),
+              const SizedBox(
+                width: 5,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  'Yes, I understand and agree to StudentHub',
+                  maxLines: 1,
+                  softWrap: false,
+                  overflow: TextOverflow.clip,
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                    //fontFamily: "GGX88Reg_Light",
+                    color: textColor,
+                  ),
+                ),
+              )
             ],
           ),
         )
