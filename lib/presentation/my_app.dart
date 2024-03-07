fb59c91d3b7dec819e93ca67a861185a72cb51c8
rayleigh420 <nhatduy0409@gmail.com>
2024-03-07 09:37:18 +0700

    UPDATE/ Button

--- lib/presentation/my_app.dart
+++ lib/presentation/my_app.dart
@@ -18,7 +18,7 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import '../di/service_locator.dart';
 import 'package:boilerplate/presentation/profile/profile_company_input.dart';
 import 'package:boilerplate/presentation/welcome/welcome_company.dart';
-import  'package:boilerplate/presentation/profile/profile_company_input_account.dart';
+import 'package:boilerplate/presentation/profile/profile_company_input_account.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -54,7 +54,7 @@ class MyApp extends StatelessWidget {
             // Built-in localization of basic text for Cupertino widgets
             GlobalCupertinoLocalizations.delegate,
           ],
-          home: ProfileCompanyInputAccount(),
+          home: ProfileCompanyInput(),
         );
       },
     );
