dfccb3205304bb6a135a56de4d9eb18292fa6e20
rayleigh420 <nhatduy0409@gmail.com>
2024-03-08 09:56:14 +0700

    ADD/ ProfileStudentInput

--- lib/presentation/profile_input/profile_input_2.dart
+++ lib/presentation/profile_input/profile_input_2.dart
@@ -1,153 +1,269 @@
+import 'dart:developer';
+
+import 'package:boilerplate/di/service_locator.dart';
+import 'package:boilerplate/presentation/home/store/theme/theme_store.dart';
+import 'package:boilerplate/core/widgets/exp_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:textfield_tags/textfield_tags.dart';
 
-class ExpItem {
-  final String title;
-  final String time;
-
-  ExpItem({required this.title, required this.time});
-}
-
-class ExpWidget extends StatefulWidget {
-  final Color borderColor;
-  final String educationText;
-  final List<ExpItem> educationItems;
-
-  ExpWidget(
-      {required this.borderColor,
-      required this.educationText,
-      required this.educationItems});
+class ProfileInput2 extends StatefulWidget {
+  const ProfileInput2({super.key});
 
   @override
-  _ExpWidgetState createState() => _ExpWidgetState();
+  State<ProfileInput2> createState() => _ProfileInput2State();
 }
 
-class _ExpWidgetState extends State<ExpWidget> {
+const List<String> techStacks = <String>[
+  'Full Stack Developer',
+  'Dev Ops',
+  'Flutter Developer',
+  'React Native Developer',
+  'Android Developer',
+  'IOS Developer',
+];
+
+class _ProfileInput2State extends State<ProfileInput2> {
+  String techStacksValue = techStacks.first;
+  final List<String> skillsets = [];
+  final TextEditingController skillSetTextController = TextEditingController();
+  final FocusNode skillSetFocusNode = FocusNode();
+  final ThemeStore _themeStore = getIt<ThemeStore>();
+  Color borderColor = Colors.grey;
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+
+    // _themeStore.changeBrightnessToDark(false);
+    if (_themeStore.darkMode) {
+      borderColor = Colors.white;
+    } else {
+      borderColor = Colors.black;
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.65,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.educationText,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
+    return SafeArea(
+        child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+          child: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 17.0),
+            Text(
+              'Experiences',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
               ),
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.only(right: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: widget.borderColor,
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        padding: const EdgeInsets.all(4.0),
-                        child: Icon(
-                          Icons.add,
-                          size: 17,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            ],
+            ),
+            const SizedBox(height: 20.0),
+            Text(
+              'Tell us about your self and you will be on your way connect with real-world project',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 20.0),
+            buildProject(context),
+
+            const SizedBox(height: 30),
+            const SizedBox(height: 30),
+            // buildNext(context),
+            // Container(
+            //   height: 500,
+            //   decoration: BoxDecoration(
+            //     border: Border.all(
+            //       color: Colors.grey,
+            //       width: 1,
+            //     ),
+            //   ),
+            // ),
+          ],
+        ),
+      )),
+    ));
+  }
+
+  void addSkillSet() {
+    final newSkillSet = skillSetTextController.text.trim();
+    if (newSkillSet.isNotEmpty && !skillsets.contains(newSkillSet)) {
+      setState(() {
+        skillsets.add(newSkillSet);
+      });
+      skillSetTextController.clear();
+      skillSetFocusNode.requestFocus();
+    }
+  }
+
+  void _removeTag(int index) {
+    setState(() {
+      skillsets.removeAt(index);
+    });
+  }
+
+  Widget buildTags(BuildContext context) {
+    return Row(
+      children: [
+        Wrap(
+          spacing: 8.0,
+          runSpacing: 8.0,
+          children: List<Widget>.generate(
+            skillsets.length,
+            (i) => buildSkillsetItem(context, i),
           ),
         ),
-        const SizedBox(height: 16),
-        for (var item in widget.educationItems) buildExpItem(context, item),
       ],
     );
   }
 
-  Widget buildExpItem(BuildContext context, ExpItem item) {
+  Widget buildSkillsetSection(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 15),
-      child: Row(
+      padding: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: borderColor,
+          width: 1,
+        ),
+      ),
+      child: Column(
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Wrap(
             children: [
-              Text(
-                item.title,
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                item.time,
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-            ],
-          ),
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.only(right: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: widget.borderColor,
-                        width: 1,
-                      ),
-                      borderRadius: BorderRadius.circular(100),
+              for (int i = 0; i < skillsets.length; i++)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Chip(
+                    label: Text(skillsets[i]),
+                    deleteIcon: Icon(
+                      Icons.close,
+                      size: 15,
                     ),
-                    padding: const EdgeInsets.all(4.0),
-                    child: Icon(
-                      Icons.edit,
-                      size: 17,
-                    ),
+                    onDeleted: () => _removeTag(i),
                   ),
-                  const SizedBox(width: 10),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: widget.borderColor,
-                        width: 1,
+                ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      focusNode: skillSetFocusNode,
+                      onTapOutside: (event) {
+                        FocusScope.of(context).unfocus();
+                      },
+                      controller: skillSetTextController,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter a tag',
                       ),
-                      borderRadius: BorderRadius.circular(100),
-                    ),
-                    padding: const EdgeInsets.all(4.0),
-                    child: Icon(
-                      Icons.delete,
-                      size: 17,
+                      onSubmitted: (_) {
+                        addSkillSet();
+                      },
                     ),
                   ),
                 ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildSkillsetItem(BuildContext context, int i) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: Chip(
+        label: Text(skillsets[i]),
+        deleteIcon: Icon(Icons.close),
+        onDeleted: () => _removeTag(i),
+      ),
+    );
+  }
+
+  Widget buildProject(BuildContext context) {
+    return ExpWidget(
+      borderColor: borderColor,
+      educationText: "Projects",
+      educationItems: [
+        ExpItem(
+            title: "Intelligent Taxi Dispatching system",
+            time: '9/2020 - 12/2020, 4 months',
+            description:
+                'It is the developer of a super-app for ride-hailing, food delivery, and digital payments services on mobile devices that operates in Singapore, Malaysia, ..'),
+        ExpItem(
+            title: "Intelligent Taxi Dispatching system",
+            time: '9/2020 - 12/2020, 4 months',
+            description:
+                'It is the developer of a super-app for ride-hailing, food delivery, and digital payments services on mobile devices that operates in Singapore, Malaysia, ..'),
+        ExpItem(
+            title: "Intelligent Taxi Dispatching system",
+            time: '9/2020 - 12/2020, 4 months',
+            description:
+                'It is the developer of a super-app for ride-hailing, food delivery, and digital payments services on mobile devices that operates in Singapore, Malaysia, ..'),
+      ],
+      isProject: true,
+    );
+  }
+
+  Widget buildNext(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Next",
+                style: TextStyle(
+                    color: _themeStore.darkMode ? Colors.black : Colors.white)),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(borderColor),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                      side: BorderSide(color: borderColor))),
+              padding: MaterialStateProperty.all(
+                EdgeInsets.symmetric(horizontal: 60, vertical: 15),
               ),
             ),
           )
         ],
       ),
+      // child: Column(
+      //   mainAxisAlignment: MainAxisAlignment.end,
+      //   children: [
+      //     Container(
+      //       alignment: Alignment.bottomCenter,
+      //       child: Text("Dont have Student Hub account?"),
+      //     ),
+      //     GestureDetector(
+      //       onTap: () {
+      //         log("push");
+      //         // Navigator.of(context)
+      //         //     .pushReplacement(MaterialPageRoute(builder: (context) {
+      //         //   return const SignUpIdentity();
+      //         // }));
+      //       },
+      //       child: const Text(
+      //         "Sign Up",
+      //         style: TextStyle(
+      //           color: Color(0xFF4285F4),
+      //           fontWeight: FontWeight.bold,
+      //         ),
+      //       ),
+      //     )
+      //   ],
+      // ),
     );
   }
 }
