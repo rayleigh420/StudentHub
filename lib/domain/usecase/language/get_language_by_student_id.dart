065cd56d9014b0edd48aab790ea62089f7748acd
rayleigh420 <nhatduy0409@gmail.com>
2024-04-02 17:05:33 +0700

    UPDATE/ API for language

--- lib/domain/usecase/language/get_language_by_student_id.dart
+++ lib/domain/usecase/language/get_language_by_student_id.dart
@@ -2,14 +2,13 @@ import 'package:boilerplate/core/domain/usecase/use_case.dart';
 import 'package:boilerplate/domain/entity/language/language_student_list.dart';
 import 'package:boilerplate/domain/repository/languages/language_repository.dart';
 
-class GetLanguageByStudentIdUseCase
-    extends UseCase<LanguageStudentList, LanguageStudentList> {
+class GetLanguageByStudentIdUseCase extends UseCase<LanguageStudentList, void> {
   final LanguageRepository _languageRepository;
 
   GetLanguageByStudentIdUseCase(this._languageRepository);
 
   @override
-  Future<LanguageStudentList> call({required LanguageStudentList params}) {
-    return _languageRepository.updateLanguageByStudentId(params);
+  Future<LanguageStudentList> call({required params}) {
+    return _languageRepository.getLanguageByStudentId();
   }
 }
