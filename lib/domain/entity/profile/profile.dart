f5468974e73756622ab7e1a4f5ded792bb2f8113
tklongg <klong31122001@gmail.com>
2024-04-04 21:38:13 +0700

    test

---
+++ lib/domain/entity/profile/profile.dart
@@ -0,0 +1,37 @@
+import 'package:boilerplate/domain/entity/profile/company.dart';
+import 'package:boilerplate/domain/entity/profile/student.dart';
+
+class Profile {
+  int id;
+  String fullname;
+  List<String> roles;
+  Student? student;
+  Company? company;
+  Profile({
+    required this.id,
+    required this.fullname,
+    required this.roles,
+    this.student,
+    this.company,
+  });
+
+  factory Profile.fromJson(Map<String, dynamic> map) {
+    return Profile(
+      id: map['id'],
+      fullname: map['fullname'],
+      roles: List<String>.from(map['roles']),
+      student: map['student'] != null ? Student.fromJson(map['student']) : null,
+      company: map['company'] != null ? Company.fromJson(map['company']) : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'fullname': fullname,
+      'roles': roles,
+      'student': student?.toJson(),
+      'company': company?.toJson(),
+    };
+  }
+}
