e4c8f165a56a14039f9586c85c6b7327c1d41851
rayleigh420 <nhatduy0409@gmail.com>
2024-04-23 10:23:32 +0700

    Fix/ showmodal in dashoard student project

--- lib/core/widgets/projects/submited_project_item.dart
+++ lib/core/widgets/projects/submited_project_item.dart
@@ -1,3 +1,4 @@
+import 'package:boilerplate/core/widgets/project_modal.dart';
 import 'package:boilerplate/domain/entity/project_2/project.dart';
 import 'package:flutter/material.dart';
 
@@ -12,77 +13,93 @@ class SubmitedProjectItem extends StatefulWidget {
 class _SubmitedProjectItemState extends State<SubmitedProjectItem> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.fromLTRB(12, 16, 8, 16),
-        margin: EdgeInsets.fromLTRB(0, 15, 0, 0),
-        decoration: BoxDecoration(
-          color: Theme.of(context).brightness == Brightness.dark
-              ? Color(0xFF2e3239)
-              : Colors.white,
-          borderRadius: BorderRadius.circular(8),
-          border: Border.all(color: Colors.grey, width: 0.6),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              offset: Offset(0, 2),
-              spreadRadius: 2,
-              blurRadius: 2,
-            )
-          ],
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              // decoration: BoxDecoration(
-              //   border: Border.all(color: Colors.grey),
-              //   borderRadius: BorderRadius.circular(8),
-              // ),
-              width: MediaQuery.of(context).size.width * 0.8,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.projDat.title!,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.blueAccent,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    "Submitted 3 days ago",
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text("Student are looking for",
-                      style: TextStyle(fontSize: 13)),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: 2,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.fromLTRB(10.0, 0, 0, 5.0),
-                        child: Text("-   ${widget.projDat.description!}",
-                            style: TextStyle(fontSize: 13)),
-                      );
-                    },
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                ],
-              ),
+    return GestureDetector(
+        onTap: () {
+          showModalBottomSheet(
+            isDismissible: true,
+            context: context,
+            isScrollControlled: true,
+            useRootNavigator: true,
+            enableDrag: true,
+            backgroundColor: Colors.transparent,
+            builder: (context) {
+              return ProjectModal(
+                project: widget.projDat,
+              );
+            },
+          );
+        },
+        child: Container(
+            padding: EdgeInsets.fromLTRB(12, 16, 8, 16),
+            margin: EdgeInsets.fromLTRB(0, 15, 0, 0),
+            decoration: BoxDecoration(
+              color: Theme.of(context).brightness == Brightness.dark
+                  ? Color(0xFF2e3239)
+                  : Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: Colors.grey, width: 0.6),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  offset: Offset(0, 2),
+                  spreadRadius: 2,
+                  blurRadius: 2,
+                )
+              ],
             ),
-          ],
-        ));
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  // decoration: BoxDecoration(
+                  //   border: Border.all(color: Colors.grey),
+                  //   borderRadius: BorderRadius.circular(8),
+                  // ),
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.projDat.title!,
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.blueAccent,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Submitted 3 days ago",
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const Text("Student are looking for",
+                          style: TextStyle(fontSize: 13)),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: 2,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.fromLTRB(10.0, 0, 0, 5.0),
+                            child: Text("-   ${widget.projDat.description!}",
+                                style: TextStyle(fontSize: 13)),
+                          );
+                        },
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )));
   }
 }
